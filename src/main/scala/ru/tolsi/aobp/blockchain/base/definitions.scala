b3e5cb935d39e0646eb401c0176a92e097592382
Sergey Tolmachev <tolsi.ru@gmail.com>
2016-10-28 20:17:29 +0300

    WIP on base definitions

--- src/main/scala/ru/tolsi/aobp/blockchain/base/definitions.scala
+++ src/main/scala/ru/tolsi/aobp/blockchain/base/definitions.scala
@@ -8,20 +8,21 @@ sealed trait Validable
 sealed trait ValidationError[V <: Validable]
 
 abstract class Block(id: Block#Id) extends Validable {
-  type Id = String
+  type Id = Array[Byte]
 }
 
+sealed trait ProtocolRequest
 abstract class Transaction(id: Transaction#Id) extends Validable {
-  type Id = String
+  type Id = Array[Byte]
 }
 
-abstract class BlockGenerator {
-  def blocks: Observable[Block]
+abstract class BlockGenerator[B <: Block] {
+  def blocks: Observable[B]
 }
 
 sealed trait TransactionValidationError[T <: Transaction] extends ValidationError[T]
 
-class AggregatedValidatorOnBlockchain[V <: Validable, VE <: ValidationError[V], T <: Transaction, B <: Block, BC <: BlockChain[B, T]](validators: Seq[ValidatorOnBlockchain[V, T, VE, B, BC]]) extends ValidatorOnBlockchain[V, T, VE, B, BC] {
+class AggregatedValidatorOnBlockchain[V <: Validable, VE <: ValidationError[V], T <: Transaction, B <: Block, BC <: BlockChain[B, T]](validators: Seq[ValidatorOnBlockChain[V, T, VE, B, BC]]) extends ValidatorOnBlockChain[V, T, VE, B, BC] {
   // http://stackoverflow.com/questions/7230999/how-to-reduce-a-seqeithera-b-to-a-eithera-seqb
   private def sequence[A, B](s: Seq[Either[A, B]]): Either[Seq[A], B] =
     s.foldRight(Left(Nil): Either[List[A], B]) {
@@ -34,49 +35,60 @@ class AggregatedValidatorOnBlockchain[V <: Validable, VE <: ValidationError[V], 
   }
 }
 
-abstract class TransactionValidator[T <: Transaction, B <: Block, BC <: BlockChain[B, T]] extends ValidatorOnBlockchain[T, T, TransactionValidationError[T], B, BC]
+abstract class TransactionValidator[T <: Transaction, B <: Block, BC <: BlockChain[B, T]] extends ValidatorOnBlockChain[T, T, TransactionValidationError[T], B, BC]
 
 sealed trait BlockValidatorError[B <: Block] extends ValidationError[B]
 
-abstract class BlockValidator[T <: Transaction, B <: Block, BC <: BlockChain[B, T]] extends ValidatorOnBlockchain[B, T, BlockValidatorError[B], B, BC]
+abstract class BlockValidator[T <: Transaction, B <: Block, BC <: BlockChain[B, T]] extends ValidatorOnBlockChain[B, T, BlockValidatorError[B], B, BC]
 
-abstract class ValidatorOnBlockchain[V <: Validable, T <: Transaction, E <: ValidationError[V], B <: Block, BC <: BlockChain[B, T]] {
+abstract class ValidatorOnBlockChain[V <: Validable, T <: Transaction, E <: ValidationError[V], B <: Block, BC <: BlockChain[B, T]] {
   def validate(tx: V)(implicit blockChain: BC): Either[Seq[E], V]
 }
 
 // todo хранить блокчейн как дерево и удалять неосновные ветки после N
 abstract class BlockStorage[B <: Block] {
-  def put(block: Block): Unit
+  def put(block: B): Unit
 
   def get(id: Block#Id): B
 
   def contains(id: Block#Id): Boolean
 }
 
-abstract class TransactionStorage[T <: Transaction] {
-  def put(block: Block): Unit
+abstract class UnconfirmedTransactionStorage[T <: Transaction] {
+  def put(tx: T): Unit
 
-  def get(id: Transaction#Id): T
+  def all: Seq[T]
 
-  def contains(id: Transaction#Id): Boolean
+  def remove(tx: T): Option[T]
 }
 
-abstract class Account
+abstract class Account[BC <: BlockChain[_,_]]
 
-abstract class Wallet {
-  def createNewAccount: Account
+abstract class Wallet[BC <: BlockChain[_,_]] {
+  def createNewAccount: Account[BC]
 }
 
 trait BlockChain[B <: Block, T <: Transaction] {
   def genesis: Block
 
-  def wallet: Wallet
+  def hashes: Array[CryptographicHash]
+
+  def txValidator: AggregatedValidatorOnBlockchain[T, TransactionValidationError[T], T, B, this.type]
+
+  def blockValidator: AggregatedValidatorOnBlockchain[B, BlockValidatorError[B], T, B, this.type]
+}
+
+
+trait BlockChainApp[B <: Block, T <: Transaction, BC <: BlockChain[B, T]] {
+  def blockChain: BC
+
+  def wallet: Wallet[BC]
 
   def storage: BlockStorage[B]
 
-  def utx: Seq[T]
+  def utx: UnconfirmedTransactionStorage[T]
 
-  def miner: BlockGenerator
+  def miner: BlockGenerator[B]
 
   def hashes: Seq[CryptographicHash]
 
@@ -86,13 +98,15 @@ trait BlockChain[B <: Block, T <: Transaction] {
 }
 
 trait IncomingNetworkLayer {
+  def incomingRequests: Observable[ProtocolRequest]
   def incomingTx: Observable[Transaction]
   def incomingBlocks: Observable[Block]
 }
 
 trait OutgoingNetworkLayer {
-  def outcomingTx: Observable[Transaction]
-  def outcomingBlocks: Observable[Block]
+  def outgoingRequests: Observable[ProtocolRequest]
+  def outgoingTx: Observable[Transaction]
+  def outgoingBlocks: Observable[Block]
 }
 
 trait NetworkLayer extends IncomingNetworkLayer with OutgoingNetworkLayer
