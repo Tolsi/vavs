6436c517751c61784fad2f10031205f57c640818
Sergey Tolmachev <tolsi.ru@gmail.com>
2016-11-04 23:02:19 +0300

    Added some waves params

--- src/main/scala/ru/tolsi/aobp/blockchain/waves/WavesConfiguration.scala
+++ src/main/scala/ru/tolsi/aobp/blockchain/waves/WavesConfiguration.scala
@@ -12,10 +12,14 @@ trait WavesConfiguration {
   trait Configuration {
     def chainId: Byte
     def initialBaseTarget: Long
+    def initialBalance: Long
 
     def genesisGenerationSignature: Signature32
     def genesisGeneratorAccount: Account
     def genesisSignature: Signature64
+
+    def totalWaves: Long
+    def unitsInWave: Long
 
     def maxTimeDriftMillis: Long
     def maxTimeForUnconfirmedMillis: Long
@@ -25,11 +29,15 @@ trait WavesConfiguration {
 
   abstract class DefaultConfiguration extends Configuration {
     override val initialBaseTarget = 153722867L
+    override val totalWaves = 100000000L
+    override val unitsInWave = 100000000L
+    override val initialBalance = totalWaves * unitsInWave
 
     override val maxTimeDriftMillis = 15.seconds.toMillis
     override val maxTimeForUnconfirmedMillis = 90.minutes.toMillis
     override val maxTxAndBlockDiffMillis = 2.hour.toMillis
     override val maxTransactionsPerBlock = 100
+
 
     override val genesisGenerationSignature = new Signature32(Array.fill(32)(0.toByte))
     override val genesisGeneratorAccount: Account = Account(Array.fill(32)(0.toByte))
