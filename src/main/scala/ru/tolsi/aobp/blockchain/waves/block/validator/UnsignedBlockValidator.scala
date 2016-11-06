b391153f05f97075e506385b50e29aa2fa52a34f
Sergey Tolmachev <tolsi.ru@gmail.com>
2016-11-06 22:28:31 +0300

    Transactions ordering validation

--- src/main/scala/ru/tolsi/aobp/blockchain/waves/block/validator/UnsignedBlockValidator.scala
+++ src/main/scala/ru/tolsi/aobp/blockchain/waves/block/validator/UnsignedBlockValidator.scala
@@ -2,9 +2,10 @@ package ru.tolsi.aobp.blockchain.waves.block.validator
 
 import ru.tolsi.aobp.blockchain.base._
 import ru.tolsi.aobp.blockchain.waves.WavesBlockChain
-import ru.tolsi.aobp.blockchain.waves.block.validator.error.{WrongState, WrongTransaction}
+import ru.tolsi.aobp.blockchain.waves.block.validator.error.{WrongState, WrongTransaction, WrongTransactionsOrder}
 import ru.tolsi.aobp.blockchain.waves.block.{BaseBlock, GenesisBlock}
 import ru.tolsi.aobp.blockchain.waves.transaction.validator.SignedTransactionWithTimeValidator
+import ru.tolsi.aobp.blockchain.waves.transaction.transactionsOrdering
 
 class UnsignedBlockValidator(tv: Long => SignedTransactionWithTimeValidator) extends AbstractBlockValidator[WavesBlockChain, WavesBlockChain#B] {
   private def implicitlyValidate[BL <: WavesBlockChain#B](b: BL)(implicit wbc: WavesBlockChain, validator: AbstractBlockValidator[WavesBlockChain, BL]): Either[Seq[BlockValidationError[WavesBlockChain, WavesBlockChain#B]], WavesBlockChain#B] = {
@@ -13,12 +14,21 @@ class UnsignedBlockValidator(tv: Long => SignedTransactionWithTimeValidator) ext
 
   // todo rewrite validators to returns all errors list (either concat)
   override def validate(b: WavesBlockChain#B)(implicit wbc: WavesBlockChain): Either[Seq[BlockValidationError[WavesBlockChain, WavesBlockChain#B]], WavesBlockChain#B] = {
-    val blockValidationResult = b match {
-      // todo txs in correct order
+    val lastBlockTs = wbc.stateStorage.lastBlock.block.timestamp
+    val timeValidationResult =
+      Either.cond(b.transactions.forall(tx => (lastBlockTs - tx.timestamp) <= wbc.configuration.maxTxAndBlockDiffMillis), b, Seq(new WrongTransaction("There are transactions from the past")))
+
+    val blockValidationResult = timeValidationResult.right.flatMap {
       // todo not from future and from last block time, see original waves
       case b: GenesisBlock => implicitlyValidate(b)
       case b: BaseBlock => implicitlyValidate(b)
     }
+
+    // todo hardfork params
+    val transactionsOrderValidationResult = blockValidationResult.right.flatMap(b => {
+      Either.cond(b.transactions == b.transactions.sorted, b, Seq(new WrongTransactionsOrder("Wrong transactions order")))
+    })
+
     val transactionsValidationResult = blockValidationResult.right.flatMap(b => {
       val validator = tv(b.timestamp)
       b.transactions.foldLeft[ResultT](Right[ErrorsSeqT, ValidatedT](b)) {
@@ -26,8 +36,10 @@ class UnsignedBlockValidator(tv: Long => SignedTransactionWithTimeValidator) ext
           r.right.flatMap(_ => validator.validate(t).fold[ResultT](errors => Left(errors.map(e => new WrongTransaction(e.message))), _ => r))
       }
     })
+
     val stateValidationResult = transactionsValidationResult.right.flatMap(b =>
       Either.cond(wbc.stateStorage.isLeadToValidState(b), b, Seq(new WrongState("Block transactions leads to illegal state"))))
+
     stateValidationResult
   }
 }
--- src/main/scala/ru/tolsi/aobp/blockchain/waves/transaction/package.scala
+++ src/main/scala/ru/tolsi/aobp/blockchain/waves/transaction/package.scala
@@ -7,4 +7,5 @@ package object transaction {
   implicit class SignTransaction[TX <: WavesBlockChain#T](t: TX) extends AnyRef {
     def signed(implicit wbc: WavesBlockChain): SignedTransaction[WavesBlockChain#T] = wavesTransactionSigner.sign(t)
   }
+  implicit val transactionsOrdering = new TransactionsOrdering
 }
