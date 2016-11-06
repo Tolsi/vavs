9004c8d6a5e2b0909d0f050e54ee6709a096249c
Sergey Tolmachev <tolsi.ru@gmail.com>
2016-11-06 17:49:07 +0300

    Removed useless binary serializers

--- src/main/scala/ru/tolsi/aobp/blockchain/waves/binary/serializers/NullableSerializer.scala
+++
@@ -1,7 +0,0 @@
-package ru.tolsi.aobp.blockchain.waves.binary.serializers
-
-import com.esotericsoftware.kryo.Serializer
-
-abstract class NullableSerializer[T] extends Serializer[T] {
-  override def getAcceptsNull: Boolean = true
-}
