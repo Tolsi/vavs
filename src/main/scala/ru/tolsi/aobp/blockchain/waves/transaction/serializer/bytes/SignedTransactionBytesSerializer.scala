b57e579045ee0f4ac5b4d55a726c8188a42417ce
Sergey Tolmachev <tolsi.ru@gmail.com>
2016-12-09 18:43:24 +0300

    Fixed tx serializer erasure warning

--- src/main/scala/ru/tolsi/aobp/blockchain/waves/transaction/serializer/bytes/SignedTransactionBytesSerializer.scala
+++ src/main/scala/ru/tolsi/aobp/blockchain/waves/transaction/serializer/bytes/SignedTransactionBytesSerializer.scala
@@ -6,11 +6,16 @@ import ru.tolsi.aobp.blockchain.waves.{DataForSignCreator, SignedTransaction}
 
 class SignedTransactionBytesSerializer(dataForSignCreator: DataForSignCreator[GenesisTransaction]) extends BytesSerializer[WavesSignedTransaction[WavesTransaction]] {
   override def serialize(signed: WavesSignedTransaction[WavesTransaction]): Array[Byte] = {
-    signed match {
-      case tx: SignedTransaction[GenesisTransaction] => dataForSignCreator.serialize(tx.signed)
-      case tx: SignedTransaction[PaymentTransaction] => implicitly[BytesSerializer[SignedTransaction[PaymentTransaction]]].serialize(tx)
-      case tx: SignedTransaction[IssueTransaction] => implicitly[BytesSerializer[SignedTransaction[IssueTransaction]]].serialize(tx)
-      case tx: SignedTransaction[ReissueTransaction] => implicitly[BytesSerializer[SignedTransaction[ReissueTransaction]]].serialize(tx)
+    signed.signed match {
+      case tx: GenesisTransaction =>
+        dataForSignCreator.serialize(tx)
+      //todo ???
+      case tx: PaymentTransaction =>
+        implicitly[BytesSerializer[SignedTransaction[PaymentTransaction]]].serialize(tx.asInstanceOf[SignedTransaction[PaymentTransaction]])
+      case tx: IssueTransaction =>
+        implicitly[BytesSerializer[SignedTransaction[IssueTransaction]]].serialize(tx.asInstanceOf[SignedTransaction[IssueTransaction]])
+      case tx: ReissueTransaction =>
+        implicitly[BytesSerializer[SignedTransaction[ReissueTransaction]]].serialize(tx.asInstanceOf[SignedTransaction[ReissueTransaction]])
     }
   }
 }
